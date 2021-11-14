6f15d81c68bdb8e678a1e79d85dc58ad177a3c64
Ivan Olexyn <ivan@olexyn.com>
2021-11-14 12:56:26 +0100

    Initial commit

---
+++ src/test/java/com/olexyn/field/kit/AppTest.java
@@ -0,0 +1,20 @@
+package com.olexyn.field.kit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
